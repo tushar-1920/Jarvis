4d65772ab2d53fed6e61c8b7be4d49f87b806f16
Tushar <tusharmks9816@gmail.com>
2025-09-06 14:21:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 import musicLibrary
-hfyg
+hfy
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
@@ -53,5 +53,6 @@ if __name__ == "__main__":
 
         except Exception as e:
             print(f"Error: {e}")
+
 
 
